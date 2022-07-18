025708048acebf5a5ec9fed053b38bfeae44fa50
Rishon Jonathan R <mithicalaviator85@gmail.com>
2022-07-18 18:09:42 +0530

    release variable fixed

--- src/WaveUSB-Installer/CLI/install-WaveUSB.sh
+++ src/WaveUSB-Installer/CLI/install-WaveUSB.sh
@@ -22,8 +22,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 comment
-    RELEASE="$(lsb_release -i | cut -f 2-) $(lsb_release -r | cut -f 2-)"
-    echo $RELEASE
+    RELEASE=$(lsb_release -i | cut -f 2-)$(lsb_release -r | cut -f 2-)
+
 debianDependencies(){
     sudo apt-get update && sudo apt-get full-upgrade
     sudo apt-get install wget git curl dmg2img coreutils gcc g++
