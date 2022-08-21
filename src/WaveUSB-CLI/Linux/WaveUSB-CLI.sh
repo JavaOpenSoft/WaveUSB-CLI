4f5acb0eb17f7a1a84af7e4b0dd8f3039bc2ebeb
Rishon Jonathan R <mithicalaviator85@gmail.com>
2022-08-21 16:53:20 +0530

    few additions

--- src/WaveUSB-CLI/Linux/WaveUSB-CLI.sh
+++ src/WaveUSB-CLI/Linux/WaveUSB-CLI.sh
@@ -25,46 +25,58 @@ comment
 
 #Download Links for ISO Images/Installers. As of now macOS Installers are not supported.
 
-    MACOS11="https://swcdn.apple.com/content/downloads/44/35/012-22320-A_AOID136T4U/g33x1akyjzjpkpe7z0xn7nguoakljpe3a8/InstallAssistant.pkg"
-    MACOS12="https://swcdn.apple.com/content/downloads/22/05/012-42714-A_04QVESBZ2P/lnob1ikkx0mi6yz5x339ttchkxri53151v/InstallAssistant.pkg"
-    MACOS12MBP="https://swcdn.apple.com/content/downloads/16/08/012-06873-A_636SHHRD4L/528ojpmw00mulgfjsz9k50modkj31a9v0p/InstallAssistant.pkg"
-    UBUNTU="https://releases.ubuntu.com/22.04/ubuntu-22.04-desktop-amd64.iso?_ga=2.53172328.1937538139.1658218589-1552335834.1657779817"
-    DEB64="https://cdimage.debian.org/debian-cd/current/amd64/bt-dvd/debian-11.4.0-amd64-DVD-1.iso.torrent"
-    DEB="https://cdimage.debian.org/debian-cd/current/i386/bt-dvd/debian-11.4.0-i386-DVD-1.iso.torrent"
-    DEBNET64="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.4.0-amd64-netinst.iso"
-    DEBNET="https://cdimage.debian.org/debian-cd/current/i386/iso-cd/debian-11.4.0-i386-netinst.iso"
-    UBUNTUSERVERDOWNLOAD="https://releases.ubuntu.com/22.04/ubuntu-22.04-live-server-amd64.iso"
-    ARCH="http://il.us.mirror.archlinux-br.org/iso/2022.07.01/archlinux-2022.07.01-x86_64.iso"
-    FEDORAWS64="https://download.fedoraproject.org/pub/fedora/linux/releases/36/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-36-1.5.iso"
-    FEDORAWS="https://download.fedoraproject.org/pub/fedora/linux/releases/36/Workstation/aarch64/iso/Fedora-Workstation-Live-aarch64-36-1.5.iso"
-    FEDORASERV="https://download.fedoraproject.org/pub/fedora/linux/releases/36/Server/x86_64/iso/Fedora-Server-dvd-x86_64-36-1.5.iso"
-    FEDORASERVARM="https://download.fedoraproject.org/pub/fedora/linux/releases/36/Server/aarch64/iso/Fedora-Server-dvd-aarch64-36-1.5.iso"
-    DEEPIN="https://cdimage.deepin.com/releases/20.6/deepin-desktop-community-20.6-amd64.iso"
-    MANJAROGNOME="https://download.manjaro.org/gnome/21.3.4/manjaro-gnome-21.3.4-220718-linux515.iso"
-    MANJAROXFCE="https://download.manjaro.org/xfce/21.3.4/manjaro-xfce-21.3.4-220718-linux515.iso"
-    MANJAROKDE="https://download.manjaro.org/kde/21.3.4/manjaro-kde-21.3.4-220718-linux515.iso"
-    LINUXMINTEDGE="https://mirrors.kernel.org/linuxmint/stable/20.3/linuxmint-20.3-cinnamon-64bit-edge.iso"
-    LINUXMINTXFCE="https://mirrors.kernel.org/linuxmint/stable/20.3/linuxmint-20.3-xfce-64bit.iso"
-    LINUXMINTCINN="https://mirrors.kernel.org/linuxmint/stable/20.3/linuxmint-20.3-cinnamon-64bit.iso"
-    LINUXMINTMATE="https://mirrors.kernel.org/linuxmint/stable/20.3/linuxmint-20.3-mate-64bit.iso"
-    ELEMENTARYOS="https://sgp1.dl.elementary.io/download/MTY1ODIyMTYzMg==/elementaryos-6.1-stable.20211218-rc.iso"
-    SOLUSGNOME="https://mirrors.rit.edu/solus/images/4.3/Solus-4.3-GNOME.iso"
-    SOLUSBUD="https://mirrors.rit.edu/solus/images/4.3/Solus-4.3-Budgie.iso"
-    SOLUSMATE="https://mirrors.rit.edu/solus/images/4.3/Solus-4.3-MATE.iso"
-    SOLUSKDE="https://mirrors.rit.edu/solus/images/4.3/Solus-4.3-Plasma.iso"
-    BLACKARCH="https://ftp.halifax.rwth-aachen.de/blackarch/iso/blackarch-linux-full-2021.09.01-x86_64.iso"
-    BLACKARCHMINIMUM="https://ftp.halifax.rwth-aachen.de/blackarch/iso/blackarch-linux-slim-2021.09.01-x86_64.iso"
-    BLACKARCHNET="https://ftp.halifax.rwth-aachen.de/blackarch/iso/blackarch-linux-netinst-2021.09.01-x86_64.iso"
+    macOS11="https://swcdn.apple.com/content/downloads/44/35/012-22320-A_AOID136T4U/g33x1akyjzjpkpe7z0xn7nguoakljpe3a8/InstallAssistant.pkg";
+    macOS12="https://swcdn.apple.com/content/downloads/22/05/012-42714-A_04QVESBZ2P/lnob1ikkx0mi6yz5x339ttchkxri53151v/InstallAssistant.pkg";
+    macOS12_MBP="https://swcdn.apple.com/content/downloads/16/08/012-06873-A_636SHHRD4L/528ojpmw00mulgfjsz9k50modkj31a9v0p/InstallAssistant.pkg";
+    ubuntu="https://releases.ubuntu.com/22.04/ubuntu-22.04-desktop-amd64.iso?_ga=2.53172328.1937538139.1658218589-1552335834.1657779817";
+    debian64="https://cdimage.debian.org/debian-cd/current/amd64/bt-dvd/debian-11.4.0-amd64-DVD-1.iso.torrent";
+    debian="https://cdimage.debian.org/debian-cd/current/i386/bt-dvd/debian-11.4.0-i386-DVD-1.iso.torrent";
+    debianNet64="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.4.0-amd64-netinst.iso";
+    debianNet="https://cdimage.debian.org/debian-cd/current/i386/iso-cd/debian-11.4.0-i386-netinst.iso";
+    ubuntuServer="https://releases.ubuntu.com/22.04/ubuntu-22.04-live-server-amd64.iso";
+    arch="http://il.us.mirror.archlinux-br.org/iso/2022.07.01/archlinux-2022.07.01-x86_64.iso";
+    fedoraWorkspace64="https://download.fedoraproject.org/pub/fedora/linux/releases/36/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-36-1.5.iso";
+    fedoraWorkspace="https://download.fedoraproject.org/pub/fedora/linux/releases/36/Workstation/aarch64/iso/Fedora-Workstation-Live-aarch64-36-1.5.iso";
+    fedoraServer="https://download.fedoraproject.org/pub/fedora/linux/releases/36/Server/x86_64/iso/Fedora-Server-dvd-x86_64-36-1.5.iso";
+    fedoraServer_ARM="https://download.fedoraproject.org/pub/fedora/linux/releases/36/Server/aarch64/iso/Fedora-Server-dvd-aarch64-36-1.5.iso";
+    deepin="https://cdimage.deepin.com/releases/20.6/deepin-desktop-community-20.6-amd64.iso";
+    manjaroGnome="https://download.manjaro.org/gnome/21.3.4/manjaro-gnome-21.3.4-220718-linux515.iso";
+    manjaroXFCE="https://download.manjaro.org/xfce/21.3.4/manjaro-xfce-21.3.4-220718-linux515.iso";
+    manjaroKDE="https://download.manjaro.org/kde/21.3.4/manjaro-kde-21.3.4-220718-linux515.iso";
+    linuxMintEdge="https://mirrors.kernel.org/linuxmint/stable/20.3/linuxmint-20.3-cinnamon-64bit-edge.iso";
+    linuxMintXFCE="https://mirrors.kernel.org/linuxmint/stable/21/linuxmint-21-xfce-64bit.iso";
+    linuxMintCinnamon="https://mirrors.kernel.org/linuxmint/stable/21/linuxmint-21-cinnamon-64bit.iso";
+    linuxMintMate="https://mirrors.kernel.org/linuxmint/stable/21/linuxmint-21-mate-64bit.iso";
+    elementaryOS="https://sgp1.dl.elementary.io/download/MTY1ODIyMTYzMg==/elementaryos-6.1-stable.20211218-rc.iso";
+    solusGnome="https://mirrors.rit.edu/solus/images/4.3/Solus-4.3-GNOME.iso";
+    solusBudgie="https://mirrors.rit.edu/solus/images/4.3/Solus-4.3-Budgie.iso";
+    solusMATE="https://mirrors.rit.edu/solus/images/4.3/Solus-4.3-MATE.iso";
+    solusKDE="https://mirrors.rit.edu/solus/images/4.3/Solus-4.3-Plasma.iso";
+    blackArch="https://ftp.halifax.rwth-aachen.de/blackarch/iso/blackarch-linux-full-2021.09.01-x86_64.iso";
+    blackArchMinimum="https://ftp.halifax.rwth-aachen.de/blackarch/iso/blackarch-linux-slim-2021.09.01-x86_64.iso";
+    blackArchNet="https://ftp.halifax.rwth-aachen.de/blackarch/iso/blackarch-linux-netinst-2021.09.01-x86_64.iso";
+    fedoraServerNet_ARM = "https://mirrors.tuna.tsinghua.edu.cn/fedora/releases/36/Server/aarch64/iso/Fedora-Server-netinst-aarch64-36-1.5.iso";
+    fedoraServerNet = "https://download.fedoraproject.org/pub/fedora/linux/releases/36/Server/x86_64/iso/Fedora-Server-netinst-x86_64-36-1.5.iso";
+    #appDatamacOS = "/Library/Application Support/WaveUSB/";
+    #appDataLinux = System.getProperty("user.home")+"/.WaveUSB/";
+    #appDataWindows = System.getProperty("user.home")+"/.WaveUSB/";
+    
+    macOS1015 = "https://github.com/RishonDev/WaveUSB/releases/download/macOS10.15.7/macOS-Catalina.Installer-10.15.7.tar.xz";
+    macOS1014 ="https://github.com/RishonDev/WaveUSB/releases/download/macOS10.14.6/macOS-Mojave-Installer-10.14.6.tar.xz";
+    macOS1013 = "https://github.com/RishonDev/WaveUSB/releases/download/macOS10.13.6/macOS-HighSierra-Installer-10.13.6.tar.xz";
+    writeTrigger = "0"
+    inputTrigger = "0"
+    outputTriger = "0"
   #Input file and Output device
     IMAGE=""
     DRIVE=""
     while getopts 'iohaw:' OPTION; do
   case "$OPTION" in
     i)
-       export IMAGE="$OPTARG"
+       IMAGE="$OPTARG"
        exit;;
     o)
-     export DRIVE="$OPTARG"
+     DRIVE="$OPTARG"
      exit;;  
     h)
       echo "-i Input image Like this: -i YOURIMAGEHERE"
@@ -84,7 +96,6 @@ comment
       exit;;
     
     w)
-    sudo dd if="$IMAGE" of="$DRIVE" bs=1M status=progress
     exit;;  
     ?)
       echo "script usage: $(basename \$0) [-l] [-h] [-a somevalue]" >&2
@@ -96,6 +107,18 @@ comment
 
 done
 shift "$(($OPTIND -1))"
+if[[writeTrigger="1"]]
+then
+       sudo dd if="$IMAGE" of="$DRIVE" bs=1M status=progress
+fi
+if[[inputTrigger="1"]]
+then
+       sudo dd if="$IMAGE" of="$DRIVE" bs=1M status=progress
+fi
+if[[outputTrigger="1"]]
+then
+       sudo dd if="$IMAGE" of="$DRIVE" bs=1M status=progress
+fi
 echo "Welcome to waveUSB writer!"
 echo "Let's now begin to create your bootable USB device."
 echo "Do You wish to:"
@@ -127,16 +150,79 @@ welcome(){
   echo "3)Display the program information"
   echo "4)Exit program"
 }
-imageFromOnline(){
+macOS(){
   echo "Enter the OS you would like to use:"
   echo "1)macOS 11(Version 11.6.7, Name:macOS Big Sur)"
   echo "2)macOS 12(Version 12.4, Name:macOS Monterey)"
 
 }
-macOS12(){}
+
+OS_SELECTION(){
+  echo "1.Linux"
+  echo "2.macOS"
+  echo "3.Windows"
+  echo "4.Other Image.."
+  echo "5.Back.."
+  echo "6.Exit.."
+  while true; do
+    read yn
+    case $yn in
+        [1]* ) Linux;;
+        [2]* ) macOS;;
+        [3]* ) Windows;;
+        [4]* ) Other;;
+        [5]*) welcome;;
+        [6]*) exit;;
+    esac
+done
+  
+}
+Windows(){
+  clear
+  echo "1.Windows XP 64-bit"
+  echo "2. Windows XP 32-bit"
+  echo "3.Windows 8 32-bt"
+  echo "4.Windows 8 64-bit"
+  echo "5.Windows 10 32-bit"
+  echo "6.Windows 10 64-bit"
+  echo "7.Windows 11(64-bit only)"
+  echo "8.Back.."
+  echo "9.Exit"
+  while true; do
+    read yn
+    case $yn in
+        [1]* ) wget ();;
+        [2]* ) wget();;
+        [3]* ) wget();;
+        [4]*) wget();;
+        [5]*) wget();;
+        [6]*) wget();;
+        [7]*) wget();;
+        [8]*) OS_SELECTION;;
+        [9]* ) exit;;
+    esac
+done
+}
+Linux(){
+
+}
+aboutSoftware(){
+  echo "WaveUSB CLI Version 1.0"
+  echo "Build 22.7.19"
+  echo "Edition - CLI"
+  echo "Build Mode:Staging"
+  echo "More information on https://github.com/RishonDev/WaveUSB-CLI"
+  exit
+}
+Other(){
+  read IMAGE -p "Enter the path of the image file"
+               lsblk
+  read DRIVE -p "Enter the path of the drive"
+  clear
+  echo "Writing the image to disk..."
+  sudo dd if="$IMAGE" of="$DRIVE" bs=1M status=progress
+}
 write(){
   sudo dd if="$IMAGE" of="$DRIVE" bs=1M status=progress
-}about(){
-
 }
 
